7a7b4937610b3cafc3e6288daafcb493f15c0ea5
TAMMY SURYANA <tammysuryana@gmail.com>
2020-11-20 21:58:36 +0700

    tams tambahan ke delet

---
+++ midleware/auth.js
@@ -0,0 +1,96 @@
+var connection = require ('../koneksi');
+var mysql = require ('mysql');
+var md5 = require('md5');
+var response = require ('../rest' );
+var jwt = require('jsonwebtoken');
+var config = require('../config/secret');
+var ip = require ('ip');
+
+
+//controler untuk register
+exports.register = function(req,res){
+    var post ={
+        username : req.body.username,
+        email : req.body.email,
+        password : md5(req.body.password),
+        role : req.body.role,
+        tanggal_daftar : new Date()
+    }
+
+    //untuk mengecek yang terdaftar email 
+    var query = "SELECT email FROM ?? WHERE ??=?";
+    var table = ["user", "email", post.email];
+
+    query = mysql.format(query,table);
+    connection.query(query, function (error, rows) {
+        if (error) {
+             console.log(error);
+        } else {
+             if (rows.length == 0) {
+                  var query = "INSERT INTO ?? SET ?";
+                  var table = ["user"];
+                  query = mysql.format(query, table);
+                  connection.query(query, post, function (error,rows) {
+                       if (error) {
+                            console.log(error);
+                       } else {
+                        response.ok("berhasil di tambahkan bro !".res);
+                    }
+                });
+            }else{
+                response.ok("email nya udah pernah daftar cuy !",res)
+            }
+        }
+    })
+}
+
+
+
+// controler buat login sob 
+ exports.login = function(req,res){
+     var post = {
+         password : req.body.password,
+         email: req.body.email
+     }
+     var query = "SELECT * FROM ?? WHERE ??=? AND ??=?";
+     var table =['user' , 'password', md5 (post.password),"email", post.email ];
+
+     query = mysql.format(query,table);
+     connection.query(query, function(error, rows){
+         if(error){
+             console.log(error);
+         }else{
+             if(rows.length == 1 ){
+                 var token = jwt.sign({rows}, config.secret,{
+                    expiresIn: 1440 
+                 });
+                 id_user = rows[0].id;
+
+                 var  data = {
+                     id_user : id_user,
+                     access_token : token,
+                     ip_address : ip.address()
+                 }
+                 var query = "INSERT INTO ?? SET ?";
+                 var table = ["akses_token"];
+                 query = mysql.format(query,table);
+                 connection.query(query,data , function(error, rows){
+                     if(error){
+                         console.log(error);
+                     }else{
+                         res.json({
+                             succes :true,
+                             message : 'token JWT tergeneret bro',
+                             token : token, 
+                             currUser : data.id_user
+
+                         });
+                     }
+                 });
+             }else {
+                 res.json({"error": true,"message":"emailnya salah gaes !!!" });
+             }
+         }
+     });
+ }
+
---
+++ midleware/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var auth = require('./auth'); 
+var router = express.Router();
+
+
+//daftarkan menu registrasi
+router.post('/api/v1/register', auth.register);
+router.post('/api/v1/login', auth.login);
+
+module.exports = router;
